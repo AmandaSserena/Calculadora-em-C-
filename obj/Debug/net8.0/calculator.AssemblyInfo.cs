ce498d05c0f6c1453266e0595405ba19bbafb0db
Amanda Serena <amandaserena@rede.ulbra.br>
2024-03-30 14:03:27 -0300

    first commit

--- obj/Debug/net8.0/calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4103d0d5d5a7a5e9702bc67823c59b3d30646a81")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
